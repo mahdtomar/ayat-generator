33d35c1739e10b5016d891e22382a299feb9db6e
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-06-15 18:00:56 +0200

    working viewing ayat function

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
-console.log("hello world");
-fetch("https://api.imgflip.com/get_memes");
-const request = new XMLHttpRequest();
-request.open("GET", "https://api.imgflip.com/get_memes");
-request.send();
-const image = document.createElement("img")
-// console.log(request);
+const btn = document.getElementById("btn");
+btn.onclick = function generateRandomeAyah() {
+  const request = new XMLHttpRequest();
 
-request.onreadystatechange = function () {
-  if (this.status === 200 && this.readyState == 4) {
-    let JsData = JSON.parse(request.responseText)
-    console.log(JsData.data.memes)
-    let memesArray = JsData.data.memes
-    memesArray.forEach(e => {
-        let meme = image.cloneNode()
-        let memeUrl = e.url
-        meme.setAttribute("src",memeUrl)
-        document.body.appendChild(meme)
-        console.log(e.name)
-    });
-  }
+  let ayahNumber = Math.floor(Math.random() * (6236 - 1 + 1)) + 1; // get random number between 6236(max number) and 1
+
+  request.open("GET", `http://api.alquran.cloud/v1/ayah/${ayahNumber}`);
+  request.send();
+
+  const div = document.createElement("div"); // div to make copies from later
+  const paragraph = document.createElement("p"); // p to make copies from later
+
+  request.onreadystatechange = function () {
+    if (this.status === 200 && this.readyState == 4) {
+      Boolean(document.querySelector(".ayah"))
+        ? document.querySelector(".ayah").remove()
+        : ""; // to make sure only one ayah is in the page
+
+      let JsData = JSON.parse(request.responseText);
+      let ayah = JsData.data.text;
+
+      // DOM
+      let ayahDiv = div.cloneNode();
+      let ayahText = paragraph.cloneNode();
+      ayahDiv.classList.add("ayah");
+      let ayahTextNode = document.createTextNode(ayah);
+      ayahText.appendChild(ayahTextNode);
+      ayahDiv.appendChild(ayahText);
+
+      document.body.appendChild(ayahDiv);
+    }
+  };
 };
